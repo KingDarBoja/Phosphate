39ffb1502eb75cf0d3ba2b44cd8056eb82dc4e65
KingDarBoja <stevenbojato04@gmail.com>
2020-03-28 22:14:59 -0500

    chore(language): WIP ast and lexer

---
+++ src/language/ast.nim
@@ -0,0 +1,319 @@
+## AST Module
+from source import Source
+from token_kind import TokenKind
+from ast_kind import ASTKind
+
+type Token* = ref object
+  #[
+    AST Token
+
+    Represents a range of characters 
+    represented by a lexical token within a Source.
+  ]#
+  kind*: TokenKind ## the kind of token
+  start*: int ## the character offset at which this Node begins
+  `end`*: int ## the character offset at which this Node ends
+  line*: int ## the 1-indexed line number on which this Token appears
+  column*: int ## the 1-indexed column number at which this Token begins
+  value*: string ## For non-punctuation tokens, represents the interpreted value of the token.
+  #[
+    Tokens exist as nodes in a double-linked-list amongst all tokens including
+    ignored tokens. <SOF> is always the first node and <EOF> the last.
+  ]#
+  prev*: Token
+  next*: Token
+
+proc initToken*(
+  kind: TokenKind,
+  start: int,
+  `end`: int,
+  line: int,
+  column: int,
+  prev: Token = nil,
+  value: string = ""
+): Token =
+  new(result)
+  result.kind = kind
+  result.start = start
+  result.`end` = `end`
+  result.line = line
+  result.column = column
+  result.value = value
+  result.prev = prev
+  result.next = nil
+
+type Location* = object
+  #[
+    AST Location
+
+    Contains a range of UTF-8 character offsets and token references that identify the
+    region of the source from which the AST derived.
+  ]#
+  start: int ## character offset at which this Node begins
+  `end`: int ## character offset at which this Node ends
+  startToken: Token ## Token at which this Node begins
+  endToken: Token ## Token at which this Node ends.
+  source: Source ## Source document the AST represents
+
+proc initLocation*(
+  star: int,
+  `end`: int,
+  startToken: Token,
+  endToken: Token,
+  source: Source
+): Location =
+  result.start = startToken.start
+  result.end = endToken.end
+  result.startToken = startToken
+  result.endToken = endToken
+  result.source = source
+
+## THIS IS INHERITANCE APPROACH
+
+#[
+  TODO NOTE:
+  To ensure array / seq immutability
+
+  1. make it private
+  2. create a getter and setter
+  3. (rename fields to some other name, perhaps pFields) then make procs "fields" and "`fields=`"
+  signature: proc fields(self: MyChildClass): seq[OtherType]
+  -> Dont actually make the 2nd proc to make it immutable
+  5. now it's immutable for any module that imports it
+  but you cant guarantee immutability within the same file
+
+  The rename part is just to ensure the field name doesnt clash with the proc name
+]#
+
+type Node* = ref object of RootObj ## Base AST Node
+  loc: Location
+  kind: ASTKind
+
+# Name
+
+type NameNode* = ref object of Node ## Name AST Node
+  value: string
+
+## Forward declarations
+
+# Document
+type DefinitionNode* = ref object of Node ## Base Definition Node
+
+# Values
+type ValueNode* = ref object of Node 
+
+type VariableNode* = ref object of ValueNode
+  name: NameNode
+
+# Document
+type ArgumentNode* = ref object of Node ## Argument AST Node
+  name: NameNode
+  value: ValueNode
+
+# Directives
+type DirectiveNode* = ref object of Node ## Directive AST Node
+  name: NameNode
+  arguments: seq[ArgumentNode]
+
+# Type Reference
+type TypeNode* = ref object of Node
+
+## Additional type to simulate union type like in Python
+## or NamedTypeNode | ListTypeNode in TypeScript
+## Check the link below for the reasoning behind this
+## https://forum.nim-lang.org/t/4406#27516
+type NonNullablesTypes = ref object of TypeNode
+
+type NamedTypeNode* = ref object of NonNullablesTypes
+  name: NameNode
+
+## Normal Declarations
+
+# Document
+
+type DocumentNode* = ref object of Node ## Document AST Node
+  definitions: DefinitionNode
+
+type VariableDefinitionNode* = ref object of Node
+  variable: VariableNode
+  `type`: TypeNode
+  defaultValue: ValueNode
+  directives: seq[DirectiveNode]
+
+type SelectionNode* = ref object of Node
+  directives: seq[DirectiveNode]
+
+type SelectionSetNode* = ref object of Node
+  selections: seq[SelectionNode]
+
+type ExecutableDefinitionNode* = ref object of DefinitionNode
+  name: NameNode
+  directives: seq[DirectiveNode]
+  variableDefinitions: seq[VariableDefinitionNode]
+  selectionSet: SelectionSetNode
+
+type OperationTypeNode* = enum
+  #[
+    Operation Type Enum for most operation definition
+    node types.
+  ]#
+  QUERY = "query"
+  MUTATION = "mutation"
+  SUBSCRIPTION = "subscription"
+
+type OperationDefinitionNode* = ref object of ExecutableDefinitionNode
+  operation: OperationTypeNode
+
+type FieldNode* = ref object of SelectionNode
+  alias: NameNode
+  name: NameNode
+  arguments: seq[ArgumentNode]
+  selectionSet: SelectionSetNode
+
+# Fragments
+
+type FragmentSpreadNode* = ref object of SelectionNode
+  name: NameNode
+
+type InlineFragmentNode* = ref object of SelectionNode
+  typeCondition: NamedTypeNode
+  selectionSet: SelectionSetNode
+
+type FragmentDefinitionNode* = ref object of ExecutableDefinitionNode
+  typeCondition: NamedTypeNode
+
+# Values
+
+type IntValueNode* = ref object of ValueNode
+  value: string
+
+type FloatValueNode* = ref object of ValueNode
+  value: string
+
+type StringValueNode* = ref object of ValueNode
+  value: string
+  `block`: bool
+
+type BooleanValueNode* = ref object of ValueNode
+  value: bool
+
+type NullValueNode* = ref object of ValueNode
+
+type EnumValueNode* = ref object of ValueNode
+  value: string
+
+type ListValueNode* = ref object of ValueNode
+  values: seq[ValueNode]
+
+type ObjectFieldNode* = ref object of ValueNode
+  name: NameNode
+  value: ValueNode
+
+type ObjectValueNode* = ref object of ValueNode
+  fields: seq[ObjectFieldNode]
+
+# Type Reference
+
+type ListTypeNode* = ref object of NonNullablesTypes
+  `type`: TypeNode
+
+type NonNullTypeNode* = ref object of TypeNode
+  `type`: NonNullablesTypes
+
+# Type System Definition
+
+type TypeSystemDefinitionNode* = ref object of DefinitionNode
+
+type OperationTypeDefinitionNode* = ref object of Node
+  operation: OperationTypeNode
+  `type`: NamedTypeNode
+
+type SchemaDefinitionNode* = ref object of TypeSystemDefinitionNode
+  description: StringValueNode
+  directives: seq[DirectiveNode]
+  operationTypes: OperationTypeDefinitionNode
+
+# Type Definition
+
+type TypeDefinitionNode* = ref object of TypeSystemDefinitionNode
+  description: StringValueNode
+  directives: seq[DirectiveNode]
+  name: NameNode
+
+type ScalarTypeDefinitionNode* = ref object of TypeDefinitionNode
+
+type InputValueDefinitionNode* = ref object of DefinitionNode
+  description: StringValueNode
+  name: NameNode
+  directives: seq[DirectiveNode]
+  `type`: TypeNode
+  defaultValue: ValueNode
+
+type FieldDefinitionNode* = ref object of DefinitionNode
+  description: StringValueNode
+  name: NameNode
+  directives: seq[DirectiveNode]
+  arguments: seq[InputValueDefinitionNode]
+
+type ObjectTypeDefinitionNode* = ref object of TypeDefinitionNode
+  interfaces: seq[NamedTypeNode]
+  fields: seq[FieldDefinitionNode]
+
+type InterfaceTypeDefinitionNode* = ref object of TypeDefinitionNode
+  fields: seq[FieldDefinitionNode]
+  interfaces: seq[NamedTypeNode]
+
+type UnionTypeDefinitionNode* = ref object of TypeDefinitionNode
+  types: seq[NamedTypeNode]
+
+type EnumValueDefinitionNode* = ref object of TypeDefinitionNode
+
+type EnumTypeDefinitionNode* = ref object of TypeDefinitionNode
+  values: seq[EnumValueDefinitionNode]
+
+type InputObjectTypeDefinitionNode* = ref object of TypeDefinitionNode
+  fields: seq[InputValueDefinitionNode]
+
+# Directive Definitions
+
+type DirectiveDefinitionNode* = ref object of TypeSystemDefinitionNode
+  description: StringValueNode
+  name: NameNode
+  arguments: seq[InputValueDefinitionNode]
+  repeatable: bool
+  locations: seq[NameNode]
+
+## Additional type to simulate union type
+## SchemaExtensionNode | TypeExtensionNode
+
+type TypeSystemExtensionNode* = ref object of Node
+  directives: seq[DirectiveNode]
+
+# Type System Extensions
+
+type SchemaExtensionNode* = ref object of TypeSystemExtensionNode
+  operationTypes: seq[OperationTypeDefinitionNode]
+
+# Type Extensions
+
+type TypeExtensionNode* = ref object of TypeSystemExtensionNode
+  name: NameNode
+
+type ScalarTypeExtensionNode* = ref object of TypeExtensionNode
+
+type ObjectTypeExtensionNode* = ref object of TypeExtensionNode
+  interfaces: seq[NamedTypeNode]
+  fields: seq[FieldDefinitionNode]
+
+type InterfaceTypeExtensionNode* = ref object of TypeExtensionNode
+  interfaces: seq[NamedTypeNode]
+  fields: seq[FieldDefinitionNode]
+
+type UnionTypeExtensionNode* = ref object of TypeExtensionNode
+  types: seq[NamedTypeNode]
+
+type EnumTypeExtensionNode* = ref object of TypeExtensionNode
+  values: seq[EnumValueDefinitionNode]
+
+type InputObjectTypeExtensionNode* = ref object of TypeExtensionNode
+  fields: seq[InputValueDefinitionNode]
---
+++ src/language/lexer.nim
@@ -0,0 +1,400 @@
+## Lexer Module
+import strformat
+import strutils
+
+from ast import Token, initToken
+from source import Source
+from block_string import dedentBlockStringValue
+from token_kind import TokenKind
+
+
+type EscapedChars = enum
+  BACKSPACE = '\b'
+  TABULATOR = '\t'
+  LINE_FEED = '\n'
+  FORM_FEED = '\f'
+  CARRIAGE_RETURN = '\r'
+  DOUBLE_QUOTE = '"'
+  FORWARD_SLASH = '/'
+  BACKSLASH = '\\'
+
+const PunctuactionTokenKind = {TokenKind.BANG..TokenKind.BRACE_R}
+
+proc isTokenKind(s: string): bool =
+  try:
+    discard parseEnum[TokenKind](s)
+    return true
+  except ValueError:
+    return false
+
+proc isEscapedChar(s: string): bool =
+  try:
+    discard parseEnum[EscapedChars](s)
+    return true
+  except ValueError:
+    return false
+
+proc printChar(c: char): string =
+  if len(repr(c)) != 0:
+    return repr(c)
+  else:
+    return $TokenKind.EOF
+
+proc isNameStart(c: char): bool =
+  #[
+    """Check whether char is an underscore or a plain ASCII letter"""
+  ]#
+  return IdentStartChars.contains(c)
+
+proc unicodeCharCode(s: string): int =
+  #[
+    Convert unicode string to integers.
+
+    Converts four hexadecimal chars to the integer that the string represents.
+    For example, uni_char_code('0f') will return 15,
+    and uni_char_code('0','0','f','f') returns 255.
+
+    Returns a negative number on error, if hex integer was invalid.
+
+    This is implemented by noting that strutils fromHex proc raises
+    a ValueError if `s` is not a valid hex integer. Capture the error
+    and return -1 instead.
+  ]#
+  var intValue: int
+  try:
+    intValue = fromHex[int](s)
+  except ValueError:
+    intValue = -1
+  return intValue
+
+proc unexpectedCharacterMessage(c: char): string =
+  #[
+    Report a message that an unexpected character was encountered.
+  ]#
+  if c < ' ' and c notin "\t\n\r":
+      return &"Cannot contain the invalid character {printChar(c)}."
+  if c == '\'':
+      return "Unexpected single quote character ('), did you mean to use a double quote (\")?"
+  return &"Cannot parse the unexpected character {printChar(c)}."
+
+proc isPunctuatorTokenKind*(kind: TokenKind): bool =
+  #[
+    Check whether the given token kind corresponds to a punctuator.
+
+    For internal use only.
+  ]#
+  return kind in PunctuactionTokenKind
+
+type Lexer* = object
+  #[
+    A Lexer is a stateful stream generator in that every time it is advanced, it returns
+    the next token in the Source. Assuming the source lexes, the final Token emitted by
+    the lexer will be of kind EOF, after which the lexer will repeatedly return the same
+    EOF token whenever called.
+  ]#
+  source: Source
+  lastToken: Token ## The previously focused non-ignored token.
+  token: Token ## The currently focused non-ignored token.
+  line: int ## The (1-indexed) line containing the current token.
+  lineStart: int ## The character offset at which the current line begins.
+
+proc initLexer*(source: Source): Lexer =
+  #[
+    Given a Source object, creates a Lexer for that source.
+  ]#
+  result.source = source
+  let startOfFileToken = initToken(TokenKind.SOF, 0, 0, 0, 0)
+  result.lastToken = startOfFileToken
+  result.token = startOfFileToken
+  result.line = 1
+  result.lineStart = 0
+
+proc positionAfterWhitespace*(self: var Lexer, body: string, startPosition: int): int =
+  #[
+    Go to next position after a whitespace.
+
+    Reads from body starting at start_position until it finds a non-whitespace
+    character, then returns the position of that character for lexing.
+  ]#
+  let bodyLen = body.len
+  var pos = startPosition
+  while pos < bodyLen:
+    let character = body[pos]
+    if character in " \t,\ufeff":
+      inc(pos)
+    elif character == '\n':
+      inc(pos)
+      inc(self.line)
+      self.lineStart = pos
+    elif character == '\r':
+      if body[pos + 1] == '\n':
+        inc(pos, 2)
+      else:
+        inc(pos)
+      inc(self.line)
+      self.lineStart = pos
+    else:
+      break
+  return pos
+
+proc readComment(self: Lexer, start: int, line: int, col: int, prev: Token): Token =
+  #[
+    Read a comment token from the source file.
+  ]#
+  let body = self.source.body
+  let bodyLen = body.len
+
+  var pos = start
+  while true:
+    inc(pos)
+    if pos >= bodyLen:
+      break;
+    let character = body[pos]
+    if character < ' ' and character != '\t':
+      break
+  
+  return initToken(TokenKind.COMMENT, start, pos, line, col, prev, body[start + 1..<pos])
+
+proc readName(self: Lexer, start: int, line: int, col: int, prev: Token): Token =
+  #[
+    Read an alphanumeric + underscore name from the source.
+  ]#
+  let body = self.source.body
+  let bodyLen = body.len
+  var pos = start + 1
+  while pos < bodyLen:
+    let character = body[pos]
+    if not IdentChars.contains(character):
+      break
+    inc(pos)
+  
+  return initToken(TokenKind.NAME, start, pos, line, col, prev, body[start..<pos])
+
+proc readDigits(self: Lexer, start: int, character: var char): int =
+  #[
+    Return the new position in the source after reading digits.
+  ]#
+  let source = self.source
+  let body = source.body
+  let bodyLen = body.len
+  var pos = start
+  while Digits.contains(character):
+    inc(pos)
+    if pos < bodyLen - 1:
+      character = body[pos]
+    else:
+      break
+  if pos == start:
+    # TODO: Should be GraphQLSyntaxError
+    raise newException(ValueError, &"Invalid number, expected digit but got: {printChar(character)}.")
+
+proc readNumber(self: Lexer, start: int, character: var char, line: int, col: int, prev: Token): Token =
+  #[
+    Reads a number token from the source file.
+    
+    Either a float or an int depending on whether a decimal point appears.
+  ]#
+  let source = self.source
+  let body = source.body
+  var pos = start
+  var isFloat = false
+  if character == '-':
+    inc(pos)
+    character = body[pos]
+
+  if character == '0':
+    inc(pos)
+    character = body[pos]
+    # TODO: Use Digits const from strutils
+    if Digits.contains(character):
+      # TODO: Should be GraphQLSyntaxError
+      raise newException(ValueError, &"Invalid number, unexpected digit after 0: {printChar(character)}.")
+  else:
+    pos = self.readDigits(pos, character)
+    character = body[pos]
+
+  if character == '.':
+    isFloat = true
+    inc(pos)
+    character = body[pos]
+    pos = self.readDigits(pos, character)
+    character = body[pos]
+
+  if len($character) != 0 and character in "Ee":
+    isFloat = true
+    inc(pos)
+    character = body[pos]
+    if len($character) != 0 and character in "+-":
+      inc(pos)
+      character = body[pos]
+    pos = self.readDigits(pos, character)
+    character = body[pos]
+
+  # Numbers cannot be followed by . or NameStart
+  if len($character) != 0 and (character == '.' or isNameStart(character)):
+    # TODO: Should be GraphQLSyntaxError
+    raise newException(ValueError, &"Invalid number, expected digit but got: {printChar(character)}.")
+
+  var finalTokenKind: TokenKind
+  if isFloat:
+    finalTokenKind = TokenKind.FLOAT
+  else:
+    finalTokenKind = TokenKind.INT
+  return initToken(finalTokenKind, start, pos, line, col, prev, body[start..<pos])
+
+proc readBlockString(self: var Lexer, start: int, line: int, col: int, prev: Token): Token =
+  #[
+    Reads a block string token from the source file.
+  ]#
+  let source = self.source
+  let body = source.body
+  let bodyLen = body.len
+  var pos = start + 3
+  var chunkStart = pos
+  var rawValue = ""
+
+  while pos < bodyLen:
+    var character = body[pos]
+    if character == '"' and body[pos + 1..<pos + 3] == "\"\"":
+      rawValue = rawValue & body[chunkStart..<pos]
+      return initToken(TokenKind.BLOCK_STRING, start, pos + 3, line, col, prev, dedentBlockStringValue(rawValue))
+    if character < ' ' and character notin "\t\n\r":
+      # TODO: Should be GraphQLSyntaxError
+      raise newException(ValueError, &"Invalid character within String: {printChar(character)}.")
+    if character == '\n':
+      inc(pos)
+      inc(self.line)
+      self.lineStart = pos
+    elif character == '\r':
+      if body[pos + 1] == '\n':
+        inc(pos, 2)
+      else:
+        inc(pos)
+      inc(self.line)
+      self.lineStart = pos
+    elif character == '\\' and body[pos + 1..<pos + 4] == "\"\"\"":
+      rawValue = rawValue & body[chunkStart..<pos] & "\"\"\""
+      inc(pos, 4)
+      chunkStart = pos
+    else:
+      inc(pos)
+  
+  # TODO: Should be GraphQLSyntaxError
+  raise newException(ValueError, "Unterminated string.")
+
+proc readString(self: Lexer, start: int, line: int, col: int, prev: Token): Token =
+  #[
+    Read a string token from the source file.
+  ]#
+  let source = self.source
+  let body = source.body
+  let bodyLen = body.len
+  var pos = start + 1
+  var chunkStart = pos
+  var value: seq[string]
+
+  while pos < bodyLen:
+    var character = body[pos]
+    if character in "\n\r":
+      break
+    if character == '"':
+      value.add(body[chunkStart..<pos])
+      return initToken(TokenKind.STRING, start, pos + 1, line, col, prev, join([value]))
+    if character < ' ' and character != '\t':
+      # TODO: Should be GraphQLSyntaxError
+      raise newException(ValueError, &"Invalid character within String: {printChar(character)}.")
+
+    inc(pos)
+    if character == '\\':
+      value.add(body[chunkStart..<pos - 1])
+      character = body[pos]
+      let isEscaped = isEscapedChar($character)
+      if isEscaped:
+        value.add($character)
+      elif character == 'u' and pos + 4 < bodyLen:
+        let code = unicodeCharCode(body[pos + 1..<pos + 5])
+        if code < 0:
+          var escape = repr(body[pos..<pos + 5])
+          escape = escape[0] & "\\" & escape[1..^1]
+          # TODO: Should be GraphQLSyntaxError
+          raise newException(ValueError, &"Invalid character escape sequence: {escape}.")
+        value.add(toHex[int](code))
+        inc(pos, 4)
+      else:
+        var escape = repr(character)
+        escape = escape[0] & "\\" & escape[1..^1]
+        # TODO: Should be GraphQLSyntaxError
+        raise newException(ValueError, &"Invalid character escape sequence: {escape}.")
+      inc(pos)
+      chunkStart = pos
+  # TODO: Should be GraphQLSyntaxError
+  raise newException(ValueError, "Unterminated string.")
+
+proc readToken*(self: var Lexer, prev: Token): Token =
+  #[
+    Get the next token from the source starting at the given position.
+
+    This skips over whitespace until it finds the next lexable token, then lexes
+    punctuators immediately or calls the appropriate helper function for more
+    complicated tokens.
+  ]#
+  let source = self.source
+  let body = source.body
+  let bodyLen = body.len
+
+  # TODO: Check pos value
+  let pos = self.positionAfterWhitespace(body, prev.`end`)
+  let line = self.line
+  # TODO: Check col value
+  let col = 1 + pos - self.lineStart
+
+  if pos >= bodyLen:
+    return initToken(TokenKind.EOF, bodyLen, bodyLen, line, col, prev)
+
+  var character = body[pos]
+  let isToken = isTokenKind($character)
+  var kind: bool = false
+  if isToken:
+    kind = isPunctuatorTokenKind(parseEnum[TokenKind]($character))
+
+  if kind:
+    let parsedTokenKind = parseEnum[TokenKind]($character)
+    return initToken(parsedTokenKind, pos, pos + 1, line, col, prev)
+  elif character == '#':
+    return self.readComment(pos, line, col, prev)
+  elif character == '.':
+    if body[pos + 1..<pos + 3] == "..":
+      return initToken(TokenKind.SPREAD, pos, pos + 3, line, col, prev)
+  elif IdentStartChars.contains(character):
+    return self.readName(pos, line, col, prev)
+  elif Digits.contains(character) or character == '-':
+    return self.readNumber(pos, character, line, col, prev)
+  elif character == '"':
+    if body[pos + 1..<pos + 3] == "\"\"":
+      return self.readBlockString(pos, line, col, prev)
+    return self.readString(pos, line, col, prev)
+  # TODO: Should be GraphQLSyntaxError
+  raise newException(ValueError, unexpectedCharacterMessage(character))
+
+proc lookahead*(self: var Lexer): Token =
+  #[
+    Look ahead and return the next non-ignored token, 
+    but do not change state.
+  ]#
+  var token = self.token
+  if token.kind != TokenKind.EOF:
+    while true:
+      if isNil token.next:
+        token.next = self.readToken(token)
+      token = token.next
+      if token.kind != TokenKind.COMMENT:
+        break
+  return token
+
+proc advance*(self: var Lexer): Token =
+  #[
+    Advance the token stream to the next non-ignored token.
+  ]#
+  self.lastToken = self.token
+  self.token = self.lookahead()
+  return self.token
