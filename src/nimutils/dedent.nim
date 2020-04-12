df3659daa5aa30cd8226021c89a2aab03dafc60e
KingDarBoja <stevenbojato04@gmail.com>
2020-04-11 21:34:58 -0500

    fix(nimutils): dedent proc working as Python impl

--- src/nimutils/dedent.nim
+++ src/nimutils/dedent.nim
@@ -1,4 +1,4 @@
-import strutils
+import re, strutils
 
 proc dedent*(text: string): string =
   #[
@@ -6,8 +6,43 @@ proc dedent*(text: string): string =
 
     Also removes leading newlines and trailing spaces and tabs, 
     but keeps trailing newlines.
+
+    Thanks to Leorize for optimizing the code and making it
+    work properly with the string comparison.
   ]#
-  return unindent(text
-    .strip(trailing = false, chars = {'\n'})
-    .strip(leading = false, chars = {' ', '\t'})  
-  )
+  # use {.global.} to avoid recompiling the regex multiple times
+  let leadingWhitespace {.global.} = re(r"(^[ \t]*)(?:[^ \t\n])", {reMultiline})
+
+  result = text.strip(trailing = false, chars = {'\n'})
+               .strip(leading = false, chars = {' ', '\t'})
+  
+  # Look for the longest leading string of spaces 
+  # and tabs common to all lines.
+  var margin: string
+
+  for indent in result.findAll(leadingWhitespace):
+    if margin.len == 0:
+      margin = indent
+
+    # Current line more deeply indented than previous winner:
+    # no change (previous winner is still on top).
+    elif indent.startsWith(margin):
+      discard
+
+    # Current line consistent with and no deeper than previous winner:
+    # it's the new winner.
+    elif margin.startsWith(indent):
+      margin = indent
+
+    # Find the largest common whitespace between current line and previous
+    # winner.
+    else:
+      # compare each character in margin and indent and cut off margin if 
+      # indent contain a character different from what's in margin
+      for i in 0 .. min(margin.high, indent.high):
+        if margin[i] != indent[i]:
+          margin = margin[0 ..< i]
+          break
+
+  if margin.len > 0:
+    result = result.replace(re("(?m)^" & margin), "")
