76af6ad1452eb0690a01cbcdaa5e03b62e270293
KingDarBoja <stevenbojato04@gmail.com>
2020-08-02 19:52:15 -0500

    chore(language): get visitor logic

--- src/language/visitor.nim
+++ src/language/visitor.nim
@@ -171,7 +171,7 @@ type
         - IDLE = undefined  -> no action
       
       Any value will replace the node with the returned value.
-    ]## 
+    ]##
     vcBreak, vcSkip, vcRemove, vcIdle
 
   VisitFuncParams* = object
@@ -181,11 +181,11 @@ type
     ]##
     node*: GraphNode
     ## The current node being visiting.
-    key*: int
+    key*: GraphNodeKind
     ## The index or key to this node from the parent node or Array.
     parent*: GraphNode
     ## The parent immediately above this node, which may be an Array.
-    path*: seq[int]
+    path*: seq[GraphNodeKind]
     ## The key path to get to this node from the root node.
     ancestors*: seq[GraphNode]
     ##[
@@ -194,7 +194,8 @@ type
       Note: ancestors includes arrays which contain the parent of visited node.
     ]##
 
-  VisitFun* = proc (p: VisitFuncParams): tuple[cmd: VisitorCommand, rn: GraphNode]
+  VisitFunTuple* = tuple[vc: VisitorCommand, gn: GraphNode]
+  VisitFun* = proc (p: VisitFuncParams): VisitFunTuple
 
   NamedVisitFuncs* = object
     kind*: VisitFun       # 1. Named visitors triggered when entering a node of a specific kind.
@@ -208,23 +209,32 @@ type
     enterKindMap*: Table[GraphNodeKind, VisitFun]  # 4. Parallel visitors for entering and leaving nodes of a specific kind.
     leaveKindMap*: Table[GraphNodeKind, VisitFun]  # 4. Parallel visitors for entering and leaving nodes of a specific kind.
 
-  VisitorEdit* = ref object
-    key*: int
-    value*: GraphNode
+  VisitorEdit* = tuple[key: GraphNodeKind, value: GraphNode]
 
   VisitorStack* = ref object
     index*: int
-    keys*: seq[string]
+    keys*: seq[GraphNodeKind]
     edits*: seq[VisitorEdit]
     inSlice*: bool
     prev*: VisitorStack
 
 
+proc newVisitorStack*(
+  inSlice: bool,
+  index: int,
+  keys: seq[GraphNodeKind],
+  edits: seq[VisitorEdit],
+  prev: VisitorStack
+): VisitorStack =
+  new(result)
+  VisitorStack(index: index, keys: keys, edits: edits, inSlice: inSlice, prev: prev)
+
+
 proc initVisitFuncParams*(
   node: GraphNode = nil,
-  key = 0,
+  key: GraphNodeKind = gnkEmpty,
   parent: GraphNode = nil,
-  path: seq[int] = @[],
+  path: seq[GraphNodeKind] = @[],
   ancestors: seq[GraphNode] = @[]
 ): VisitFuncParams =
   VisitFuncParams(node: node, key: key, parent: parent, path: path, ancestors: ancestors)
@@ -277,18 +287,33 @@ proc getVisitFn*(
     # Generic Visitor
     if not visitorOpts.enter.isNil:
       # { enter() {} }
-      echo "-- GENERIC: Check if has enter! --"
+      # echo "-- GENERIC: Check if has enter! --"
       result = visitorOpts.enter
     if visitorOpts.enterKindMap.hasKey(kind):
       # { enter: { Kind() {} } }
       result = visitorOpts.leaveKindMap[kind]
         
 
+proc popNodeSlice[T](a: seq[seq[T]]): (seq[T], seq[seq[T]]) =
+  if a.len == 0:
+    result = (@[], @[])
+  else:
+    result = (a[^1], a[0..^1])
+
+
+proc removeNodeByIndex[T](a: seq[T], pos: int): seq[T] =
+  if pos < 0 or pos >= a.len:
+    result = a
+  else:
+    result = a & a[pos + 1..^1]
+
+
+
 proc visit*(
   root: GraphNode,
   visitor: VisitorOptions,
   # visitorKeys: seq[GraphNodeKind] = @[]
-): (VisitorCommand, GraphNode) =
+): VisitFunTuple =
   ##[
     Visit each node in an AST.
 
@@ -311,42 +336,81 @@ proc visit*(
     parentSlice: seq[GraphNode]
     inSlice = false
     prevInSlice = false
-    keys: seq[int] = @[root.kind.ord]
+    keys: seq[GraphNodeKind] = @[root.kind]
     index = -1
     edits: seq[VisitorEdit]
-    path: seq[int]
+    path: seq[GraphNodeKind]
     ancestors: seq[GraphNode]
     ancestorsSlice: seq[seq[GraphNode]]
     newRoot = root
 
+  echo fmt"Initial Index: {index}"
+  # while true:
   while true:
     inc(index)
-    echo fmt"Index: {index}"
-    let
+    echo "-------------------------------------------------------"
+    echo fmt"{index} - Current Iteration"
+    echo fmt"{index} - Keys Len: {keys.len} - Current Node Kind: {root.kind}"
+    echo fmt"{index} - Keys: @[" & keys.join(", ") & "]"
+    var
       isLeaving = index == keys.len
       isEdited = isLeaving and edits.len > 0
     var
-      key: int
+      key: GraphNodeKind
       node: GraphNode
-      parent: GraphNode
+      nodeSlice: seq[GraphNode]
 
     if isLeaving:
-      key = if ancestors.len > 0: path[^1] else: -1
+      echo fmt"{index} - Is Leaving"
+      echo fmt"{index} - Path: " & map(path, proc (n: GraphNodeKind): string = $n).join(", ")
+      key = if ancestors.len > 0: path[^1] else: gnkEmpty
       node = parent
       parent = if ancestors.len > 0: ancestors.pop() else: nil
-      echo fmt"Leaving: {root.kind}"
+      # If the Node was edited, create a copy if it is not on the original
+      # slice, otherwise, use the same node slice
+      nodeSlice = parentSlice
+      (parentSlice, ancestorsSlice) = popNodeSlice(ancestorsSlice)
+      if isEdited:
+        prevInSlice = inSlice
+        var editOffset = 0
+        for editIndex, editValue in edits.pairs:
+          if inSlice:
+            if editValue.value.isNil:
+              nodeSlice = removeNodeByIndex(nodeSlice, editIndex - editOffset)
+              inc(editOffset)
+            else:
+              nodeSlice[editIndex - editOffset] = editValue.value
+      # Create the stack after that
+      index = stack.index
+      keys = stack.keys
+      edits = stack.edits
+      inSlice = stack.inSlice
+      stack = stack.prev
     else:
+      echo fmt"{index} - Is Entering"
+      echo fmt"{index} - Path: " & map(path, proc (n: GraphNodeKind): string = $n).join(", ")
       if not parent.isNil:
+        echo fmt"{index} - Has parent - In slice: {inSlice}"
         if inSlice:
-          key = index
-          node = parent.children[key]
+          node = parent.children[index]
+          key = node.kind
         else:
           key = keys[index]
-          node = parent.children[key]
+          case parent.kind:
+          of gnkName, gnkIntValue, gnkFloatValue, gnkStringValue, gnkBooleanValue, gnkNullValue, gnkEmpty, gnkEnumValue, gnkOperationType:
+            discard
+          else:
+            for idx, child in parent.children.pairs:
+              if child.kind == key:
+                node = parent.children[idx]
+                break
+        echo fmt"{index} - Key: {key}"
+        if not node.isNil:
+          echo fmt"{index} - Node Kind: {node.kind}"
       else:
-        key = -1
+        echo fmt"{index} - Has no parent"
+        key = gnkEmpty
         node = newRoot
-      
       if node.isNil:
         continue
       if not parent.isNil:
@@ -354,15 +418,85 @@ proc visit*(
 
     if not node.isNil:
       let visitProc = getVisitFn(visitor, node.kind, isLeaving)
-      echo "-------- VISIT FUN --------"
+      # echo "-------- VISIT FUN --------"
       if not visitProc.isNil:
-        echo "Reaching code"
         result = visitProc(initVisitFuncParams(node, key, parent, path, ancestors))
+        echo fmt"{index} - Visit Fn Result: {result.vc}"
+        # echo fmt"{index} - Visit Proc Node Nil?: {result.gn.isNil}"
+        case result.vc:
+        of vcBreak:
+          # Stop visiting altogether
+          break
+        of vcSkip:
+          # Skip visiting this node if entering
+          # No action if leaving
+          if not isLeaving:
+            discard path.pop()
+            continue
+        of vcRemove:
+          # Delete this Node
+          edits.add((key, result.gn))
+          if not isLeaving:
+            if not result.gn.isNil:
+              node = result.gn 
+            else:
+              discard path.pop()
+              continue
+        of vcIdle:
+          # No Action
+          discard
+      else:
+        result = (vcIdle, nil)
+  
+    echo fmt"{index} - Is Edited: {isEdited}"
+    echo fmt"{index} - Result: {result}"
+    if result.vc == vcIdle and isEdited:
+      edits.add((key, node))
+
+    if isLeaving:
+      if path.len > 0:
+        discard path.pop()
+    else:
+      # TODO: Check what is going on here!
+      stack = newVisitorStack(inSlice, index, keys, edits, stack)
+      echo fmt"{index} - Created Stack!! - Keys: @[" & stack.keys.join(", ") & "]"
+      echo fmt"{index} - Stack in Slice?: {stack.inSlice}"
+      echo fmt"{index} - Stack Index: {stack.index}"
+      echo fmt"{index} - Node Kind: {node.kind}"
+      inSlice = nodeSlice.len > 0
+      if nodeSlice.len > 0:
+        inSlice = true
+        for nodeIdx, nodeItem in nodeSlice.pairs:
+          keys.add(nodeItem.kind)
+      else:
+        inSlice = false
+        if not node.isNil:
+          case node.kind:
+          of gnkName, gnkIntValue, gnkFloatValue, gnkStringValue, gnkBooleanValue, gnkNullValue, gnkEmpty, gnkEnumValue, gnkOperationType:
+            discard
+          else:
+            for nodeIdx, nodeItem in node.children:
+              keys.add(nodeItem.kind)
+      # keys = if inSlice: @[node.kind] else: map(node.children, proc (n: GraphNode): GraphNodeKind = n.kind)
+      index = -1
+      edits = @[]
+      if not parent.isNil:
+        ancestors.add(parent)
+      parent = node
+
+    if stack.isNil:
+      break
+    echo fmt"{index} - End Iteration"
+
+  if edits.len > 0:
+    newRoot = edits[^1].value
+
+  return (vcIdle, newRoot)
 
     # echo "------ RESULT --------"
     # echo path
     # echo repr(result)
-    break;
+    # break
 
 
 
@@ -388,7 +522,7 @@ proc visit*(
 #           # parent.children.delete(idx)
 #           # For now, setting to nil
 #           parent.children[idx] = nil
-#           break;
+#           break
 #   else:
 #     # Recursively Walk and Apply visitor to every children
 #     if newRoot.children.len > 0:
--- tests/language/test_visitor.nim
+++ tests/language/test_visitor.nim
@@ -1,4 +1,4 @@
-import unittest
+import unittest, strutils, strformat
 
 import language/ast
 import language/parser
@@ -6,38 +6,50 @@ import language/visitor
 
 suite "Visitor Test Suite":
 
+  # setup:
+  #   proc checkVisitorProcArgs(ast: GraphNode, p: VisitFuncParams) =
+
+
   test "Validates Path Argument":
     echo "----------------- Short AST -----------------"
     echo "Input: { a }"
     let ast = parse("{ a }", noLocation = true)
 
-    var visited: seq[string] = @[]
+    var visited: seq[seq[string]] = @[]
 
     proc enter(p: VisitFuncParams): (VisitorCommand, GraphNode) =
-      visited.add "enter"
-      visited.add $p.node.kind
-      echo "--------- ENTER CALL ---------"
-      echo p.path
+      # checkVisitorProcArgs(ast, p)
+      visited.add @["enter"] & p.path.join(", ")
+      # echo "--------- ENTER CALL ---------"
+      # echo "Path: " & p.path.join(", ") & fmt" Key: {p.key} - Kind: {p.node.kind}"
       result = (vcIdle, p.node)
 
-    discard visit(ast, newVisitorOptions(enter = enter))
-    echo "--------- Visited Result ----------"
-    echo visited
-
-  test "Random Test":
-    echo "----------------- Longer AST -----------------"
-    echo "Input: { a, b, c { a, b, c } }"
-    let ast = parse("{ a, b, c { a, b, c } }", noLocation = true)
-
-    var visited: seq[string] = @[]
-
-    proc enter(p: VisitFuncParams): (VisitorCommand, GraphNode) =
-      visited.add "enter"
-      visited.add $p.node.kind
-      echo "--------- ENTER CALL ---------"
-      echo p.path
+    proc leave(p: VisitFuncParams): (VisitorCommand, GraphNode) =
+      # checkVisitorProcArgs(ast, p)
+      visited.add @["leave"] & p.path.join(", ")
+      # echo "--------- ENTER CALL ---------"
+      # echo "Path: " & p.path.join(", ") & fmt" Key: {p.key} - Kind: {p.node.kind}"
       result = (vcIdle, p.node)
+
+    discard visit(ast, newVisitorOptions(enter = enter, leave = leave))
+    echo "********** Visited Result **********"
+    for i, v in visited.pairs:
+      echo fmt"{i} - {v}"
+
+  # test "Random Test":
+  #   echo "----------------- Longer AST -----------------"
+  #   echo "Input: { a, b, c { a, b, c } }"
+  #   let ast = parse("{ a, b, c { a, b, c } }", noLocation = true)
+
+  #   var visited: seq[string] = @[]
+
+  #   proc enter(p: VisitFuncParams): (VisitorCommand, GraphNode) =
+  #     visited.add "enter"
+  #     visited.add $p.node.kind
+  #     echo "--------- ENTER CALL ---------"
+  #     echo p.path
+  #     result = (vcIdle, p.node)
     
-    discard visit(ast, newVisitorOptions(enter = enter))
-    echo "--------- Visited Result ----------"
-    echo visited
+  #   discard visit(ast, newVisitorOptions(enter = enter))
+  #   echo "--------- Visited Result ----------"
+  #   echo visited
